2fddd784ef093f30c525f7e50cf2ad429241298f
Alexander <mrreads@yandex.com>
2019-07-29 00:35:30 +0300

    added css

--- index.php
+++ index.php
@@ -3,11 +3,8 @@ $path = __DIR__ . "/files/texts.txt";
 $handle = fopen($path, "r");
 $content = fread($handle, filesize($path)); 
 fclose($handle);
-
-$stihi = explode('__РАЗДЕЛИТЕЛЬ__', $content);
-
-$random = random_int(0, count($stihi));
-echo $stihi[$random];
+$poems = explode('__РАЗДЕЛИТЕЛЬ__', $content);
+$random = random_int(0, count($poems)-1);
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +13,10 @@ echo $stihi[$random];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Стихи Егора Летова</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    
+    <div class="poem"> <? echo $poems[$random]; ?> </div>
 </body>
 </html>
