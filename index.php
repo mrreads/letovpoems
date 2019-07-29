24a0f80af882aaa3b0a330d1a715b435a9a6b533
Alexander <mrreads@yandex.com>
2019-07-29 19:05:40 +0300

    added meta-tags

--- index.php
+++ index.php
@@ -15,7 +15,9 @@ $background = random_int(1,2);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Стихи Егора Летова</title>
+    <title>Случайный стих Егора Летова</title>
+    <meta name="discriction" content="Случайный стих из творчества Игоря Фёдоровича Летова, начиная с 1983 по 1993 года. Все стихотворения взяты из сборника стихов «Русское поле экспериментов».">
+    <meta name="robots" content="index, nofollow">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
